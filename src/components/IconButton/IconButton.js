29600714442c1f0e2a37d7e63e92153b6c5f44a4
GHP <g.heppel@dotsource.de>
2022-04-22 15:29:18 +0200

    Fixed IconButton

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -11,64 +11,63 @@ import { ThemeContext } from "../UXPinWrapper/UXPinWrapper";
  */
 let colors = [];
 function IconButton(props) {
-  const [themeOptions, setThemeOptions] = React.useContext(ThemeContext);
-  console.log(themeOptions.theme);
-  let theme = useTheme();
-  colors = getMainColors(themeOptions.theme);
-  console.log(colors);
-
-  IconButton.propTypes = {
-    /**
-     * The icon element.
-     * @uxpinpropname  Icon
-     */
-    children: PropTypes.oneOf(iconVariants),
-
-    /**
-     * The size of the button.
-     * `small` is equivalent to the dense button styling.
-     */
-    size: PropTypes.oneOf(["small", "medium", "large"]),
-
-    /**
-     * If given, uses a negative margin to counteract the padding on one
-     * side (this is often helpful for aligning the left or right
-     * side of the icon with content above or below, without ruining the border
-     * size and shape).
-     */
-    edge: PropTypes.oneOf(["start", "end", false]),
-
-    /**
-     * The color of the component. It supports those theme colors that make sense for this component.
-     */
-    color: PropTypes.oneOf(["default", "inherit", ...colors]),
-
-    /**
-     * Aria tag for accessibility
-     */
-    ariaLabel: PropTypes.string,
-
-    /**
-     * If `true`, the button will be disabled.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * If `true`, the ripple will be disabled.
-     */
-    disableRipple: PropTypes.bool,
-
-    onClick: PropTypes.func,
-
-    /**
-     * The system prop that allows defining system overrides as well as additional CSS styles.
-     */
-    sx: PropTypes.object,
-  };
+  // const [themeOptions, setThemeOptions] = React.useContext(ThemeContext);
+  // console.log(themeOptions.theme);
+  // let theme = useTheme();
+  // colors = getMainColors(themeOptions.theme);
+  // console.log(colors);
   return (
     <IconButtonM {...props}>
       <Icon fontSize={props.size}>{props.children}</Icon>
     </IconButtonM>
   );
 }
+IconButton.propTypes = {
+  /**
+   * The icon element.
+   * @uxpinpropname  Icon
+   */
+  children: PropTypes.oneOf(iconVariants),
+
+  /**
+   * The size of the button.
+   * `small` is equivalent to the dense button styling.
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+
+  /**
+   * If given, uses a negative margin to counteract the padding on one
+   * side (this is often helpful for aligning the left or right
+   * side of the icon with content above or below, without ruining the border
+   * size and shape).
+   */
+  edge: PropTypes.oneOf(["start", "end", false]),
+
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   */
+  color: PropTypes.oneOf(["default", "inherit", ...colors]),
+
+  /**
+   * Aria tag for accessibility
+   */
+  ariaLabel: PropTypes.string,
+
+  /**
+   * If `true`, the button will be disabled.
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * If `true`, the ripple will be disabled.
+   */
+  disableRipple: PropTypes.bool,
+
+  onClick: PropTypes.func,
+
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.object,
+};
 export default IconButton;
